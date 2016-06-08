fa3d23a0151828d801ed382765d80d7f3dedf30d
Nickraz <Nickraz@yandex.ru>
2016-06-08 13:55:40 +0300

    Переменные почему-то обуляются

--- cpprep/Source.cpp
+++ cpprep/Source.cpp
@@ -4,15 +4,20 @@ using namespace std;
 
 int main()
 {
-	cout<<"Hi hi hi hi"<<endl;
+	int a = 5, b = 10, c = 0;
+	cout<<"Hi hi hi hi"<<endl<<"Testing ASM"<<endl;
 	__asm
 	{
 		push ebp
-		mov ebp, esp
-		mov eax, 4
-		mov ebx, 5
-		leave
+			mov ebp, esp
+			mov a, 4
+			mov b, 5
+			mov eax, a
+			add eax, b
+			mov c, eax
+			leave
 	}
+	cout<<"C = " <<c<<endl;
 	system("pause");
 	return 0;
 }
